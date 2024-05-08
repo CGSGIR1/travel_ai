619c86770cba774381374cd5ad4ee6847b234bff
Ilya_Rodin <krokodildandy06@gmail.com>
2024-05-08 18:16:10 +0300

    Signed-off-by: Ilya_Rodin <krokodildandy06@gmail.com>

--- ModelLoad.py
+++ ModelLoad.py
@@ -4,22 +4,33 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.schema import HumanMessage
 from langchain.chat_models.gigachat import GigaChat
+import settings
 
-idf = "Y2I5NzU5ZTMtMTZhMy00YzhmLTgwODAtNTAwZWQ1ZGEwYzMzOjU0YmY4ZjQwLWI5NTgtNDJiNi1iZGVjLWRhODI5MTY5NWEzMg=="
+def GigachatStart():
+    with open('./DataBase/documents.pkl', 'rb') as fp:
+        documents = load(fp)
+    with open('./DataBase/embeddings.pkl', 'rb') as fp:
+        embeddings = load(fp)
+    llm = GigaChat(credentials=settings.idf, verify_ssl_certs=False)
 
-with open('./DataBase/documents.pkl', 'rb') as fp:
-    documents = load(fp)
-with open('./DataBase/embeddings.pkl', 'rb') as fp:
-    embeddings = load(fp)
-llm = GigaChat(credentials=idf, verify_ssl_certs=False)
+    db = Chroma.from_documents(
+       documents,
+       embeddings,
+       client_settings=Settings(anonymized_telemetry=False),
+    )
+    #f"Ты являеешься гидом по достопримечательностям России. Тебе нужно найти, 5 самых лучших достопримечательностей в регионе {request}. Ты должен вывести только их адреса, подряд, и разделяя ТОЛЬКО значком '$'"
+    return RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+    #HumanMessage(content=user_input)
 
-db = Chroma.from_documents(
-   documents,
-   embeddings,
-   client_settings=Settings(anonymized_telemetry=False),
-)
-qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
-def AIResponse():
-   AIResponse = input("Q: ")
-   resansw =qa_chain({"query": AIResponse})
-   return resansw['result']
+def AIResponse(request, qa_chain):
+
+    FormatRequest = (
+        #f" Ты должен вывести ТОЛЬКО полные адреса без пробелов, разделяя ТОЛЬКО значком '$'. Ты являеешься гидом по достопримечательностям России."
+                     #f" Тебе нужно найти, 5 самых лучших достопримечательностей в регионе {request}."
+                    # f" Достопримечательности Псков: Псковская область, город Псков, ул. Ломоносова, 95$Псковская область, город Псков, проезд Ломоносова, 08$Псковская область, город Псков, пр. Сталина, 96$Псковская область, город Псков, пер. Домодедовская, 42$Псковская область, город Псков, пр. Косиора, 05"
+                    # f" Достопримечательности Тюмень: Тюменская область, город Тюмень, спуск Ломоносова, 77$Тюменская область, город Тюмень, ул. Гоголя, 49$Тюменская область, город Тюмень, ул. Ленина, 64$Тюменская область, город Тюмень, ул. 1905 года, 70$Тюменская область, город Тюмень, пр. Ломоносова, 54"
+                    # f" Достопримечательности {request}: "
+        f"Раскажи какие есть достопримечательности в {request}:"
+        )
+    resansw = qa_chain({"query": FormatRequest})
+    return resansw['result']
